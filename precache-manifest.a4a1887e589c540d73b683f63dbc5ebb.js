ecc928417a2a7f22d901cb5d506d3087701ff376
Jieun Kang <jieunkang101@gmail.com>
2020-04-26 12:40:28 -0700

    Updates

---
+++ precache-manifest.a4a1887e589c540d73b683f63dbc5ebb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad7b08732df6c735a378918e60414bbc",
+    "url": "/TapRoom/index.html"
+  },
+  {
+    "revision": "0f6c1a8328d97069aeae",
+    "url": "/TapRoom/static/css/main.bf0fc661.chunk.css"
+  },
+  {
+    "revision": "06e0430f2e3fbfa27dd3",
+    "url": "/TapRoom/static/js/2.19980e04.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TapRoom/static/js/2.19980e04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f6c1a8328d97069aeae",
+    "url": "/TapRoom/static/js/main.6b9ea689.chunk.js"
+  },
+  {
+    "revision": "4532bdd30722e89f0d82",
+    "url": "/TapRoom/static/js/runtime-main.c0687916.js"
+  }
+]);
